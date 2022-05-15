5109e497c55b180992d39e297e5c761bf41b6c4e
Karan Melo <karan_melo@hotmail.com>
2022-05-14 21:48:55 -0300

    feat: add skills section to main content

--- src/components/layouts/Layout/Main/Content/index.tsx
+++ src/components/layouts/Layout/Main/Content/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import SectionAbout from './SectionAbout';
+import SectionDivider from './SectionDivider';
+import SectionSkills from './SectionSkills';
 import { Container } from './styles';
 
 const Content: React.FC = () => (
   <Container>
     <SectionAbout />
+    <SectionDivider />
+    <SectionSkills />
   </Container>
 );
 
--- src/components/layouts/Layout/Main/Content/styles.tsx
+++ src/components/layouts/Layout/Main/Content/styles.tsx
@@ -1,11 +1,16 @@
 import styled from 'styled-components';
 
+import TechBackground from '../../../../../assets/tech-background.jpg';
+
 export const Container = styled.div`
   position: relative;
   margin: -60px 30px 0;
 
-  border-radius: 6px 6px 0 0;
+  border-radius: 8px 8px 0 0;
+  overflow: hidden;
 
   display: flex;
   flex-direction: column;
+
+  background: url(${TechBackground}) center no-repeat;
 `;
