9569cb0234d4229d7e5fc1e333a545c93b86b3fc
Karan Melo <karan_melo@hotmail.com>
2022-05-14 21:47:24 -0300

    feat: create divider section

---
+++ src/components/layouts/Layout/Main/Content/SectionDivider/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Section } from './styles';
+
+const SectionDivider: React.FC = () => <Section />;
+
+export default SectionDivider;
---
+++ src/components/layouts/Layout/Main/Content/SectionDivider/styles.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Section = styled.section`
+  width: 100%;
+  height: 12rem;
+
+  display: flex;
+  flex-direction: column;
+`;
