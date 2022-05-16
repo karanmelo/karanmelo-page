3b71aef258cc9f4a92f98a834ef856034ecdef0e
Karan Melo <karan_melo@hotmail.com>
2022-05-15 21:36:59 -0300

    feat: add background to section divider

--- src/components/layouts/Layout/Main/Content/SectionDivider/styles.tsx
+++ src/components/layouts/Layout/Main/Content/SectionDivider/styles.tsx
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 
+import TechBackground from '../../../../../../assets/tech-background.jpg';
+
 export const Section = styled.section`
   width: 100%;
   height: 12rem;
 
   display: flex;
   flex-direction: column;
+
+  background: url(${TechBackground}) center no-repeat;
+  background-size: cover;
 `;
