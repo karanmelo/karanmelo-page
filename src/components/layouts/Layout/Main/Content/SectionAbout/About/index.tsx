b984f84d97e064128f5a3cf9bc37096a016b0894
Karan Melo <karan_melo@hotmail.com>
2022-05-15 21:58:38 -0300

    feat: initializing description in about page

--- src/components/layouts/Layout/Main/Content/SectionAbout/About/index.tsx
+++ src/components/layouts/Layout/Main/Content/SectionAbout/About/index.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 
+import { useIsInViewport } from 'hooks';
+
 import { Container, Title, Content } from './styles';
 
-const About: React.FC = () => (
-  <Container>
-    <Title>Sobre</Title>
-    <Content>
-      <p>Desenvolvedor Fulll-Stack Javascript</p>
-      <p>Node.js | React.js | Javascript | Typescript</p>
-    </Content>
-  </Container>
-);
+const About: React.FC = () => {
+  const elementRef = React.useRef<HTMLDivElement | null>(null);
+  const isInViewport = useIsInViewport(elementRef);
+
+  const getFadeRightStyles = (visible: boolean) => ({
+    transition: 'all 1s ease-in',
+    opacity: visible ? '1' : '0',
+    transform: visible ? '' : 'translateX(-100%)',
+  });
+
+  return (
+    <Container>
+      <Title>Quem é Karan?</Title>
+      <Content ref={elementRef} style={getFadeRightStyles(isInViewport)}>
+        <p>Desenvolvedor Fulll-Stack Javascript</p>
+        <p>
+          Amante de tecnologia, entusiasta de qualidade de software e boas
+          práticas de desenvolvimento.
+        </p>
+      </Content>
+    </Container>
+  );
+};
 
 export default About;
