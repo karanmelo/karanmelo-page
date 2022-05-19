37b5af8a34430c738da9aae448df87899245e63d
Karan Melo <karan_melo@hotmail.com>
2022-05-18 21:19:29 -0300

    fix: refactor layou to support semantic implementation

--- src/components/layouts/Layout/index.tsx
+++ src/components/layouts/Layout/index.tsx
@@ -3,16 +3,13 @@ import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
 import Main from './Main';
-import { Container } from './styles';
 
-const Layout: React.FC = () => {
-  return (
-    <Container>
-      <Header />
-      <Main />
-      <Footer />
-    </Container>
-  );
-};
+const Layout: React.FC = () => (
+  <>
+    <Header />
+    <Main />
+    <Footer />
+  </>
+);
 
 export default Layout;
