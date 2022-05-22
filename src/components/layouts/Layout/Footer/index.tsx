bde175b4061e180519dd681df3c069beb7ee1d07
Karan Melo <karan_melo@hotmail.com>
2022-05-22 20:34:04 -0300

    fix: update footer content

--- src/components/layouts/Layout/Footer/index.tsx
+++ src/components/layouts/Layout/Footer/index.tsx
@@ -5,6 +5,6 @@ import * as S from './styles';
 export const Footer: React.FC = () => (
   <S.Footer>
     <p>© Copyright 2022 Karan Melo. Todos os direitos reservados.</p>
-    <p>Desenvolvido com React.Js e styled-components.</p>
+    <p>Desenvolvido com ReactJS.</p>
   </S.Footer>
 );
