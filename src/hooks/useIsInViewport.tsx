d2149f8e57e28441c37ab6ebd969782affe907b5
Karan Melo <karan_melo@hotmail.com>
2022-05-22 21:23:30 -0300

    fix: update ref paramenter type of  useIsInViewport

--- src/hooks/useIsInViewport.tsx
+++ src/hooks/useIsInViewport.tsx
@@ -1,9 +1,12 @@
 import { useState, useEffect, useCallback } from 'react';
 
-export const useIsInViewport = (ref: React.MutableRefObject<any>) => {
+export const useIsInViewport = (
+  ref: React.MutableRefObject<Element | null>
+) => {
   const [isInViewPort, setIsInViewport] = useState(false);
 
   const handleScroll = useCallback(() => {
+    if (!ref.current) return false;
     const { bottom } = ref.current.getBoundingClientRect();
     return setIsInViewport(window.innerHeight - bottom > 20);
   }, [ref]);
