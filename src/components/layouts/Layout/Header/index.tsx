821d3ad5a270181a2f90c5d7db928625b727f4a7
Karan Melo <karan_melo@hotmail.com>
2022-05-23 22:20:08 -0300

    fix: handling executions with disassembled components

--- src/components/layouts/Layout/Header/index.tsx
+++ src/components/layouts/Layout/Header/index.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
+import { useIsMounted } from 'hooks/useIsMounted';
+
 import Home from './Home';
 import Menu from './Menu';
 import * as S from './styles';
@@ -7,15 +9,18 @@ import * as S from './styles';
 export const Header: React.FC = () => {
   const [active, setActive] = useState(true);
 
+  const isMounted = useIsMounted();
+
   useEffect(() => {
     window.addEventListener(
       'scroll',
       () => {
+        if (!isMounted.current) return;
         setActive(window.scrollY === 0);
       },
       false
     );
-  }, [active]);
+  }, [active, isMounted]);
 
   return (
     <S.Header active={active}>
